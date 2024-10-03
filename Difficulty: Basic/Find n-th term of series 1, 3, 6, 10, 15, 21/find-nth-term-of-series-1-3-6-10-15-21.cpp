a34807b33b609366bcb44967dfcae88399e159b9
Santhosh Bingi <110170225+santhoshbingi@users.noreply.github.com>
2024-10-03 10:00:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find n-th term of series 1, 3, 6, 10, 15, 21/find-nth-term-of-series-1-3-6-10-15-21.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findNthTerm(int N) {
+       return (N*(N + 1))/2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findNthTerm(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
