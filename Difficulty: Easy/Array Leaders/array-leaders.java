50e3fad7a936803b1cc91cef857a5ab86c40e3dc
Santhosh Bingi <110170225+santhoshbingi@users.noreply.github.com>
2026-01-02 16:48:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -1,15 +1,30 @@
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
         // code here
-        ArrayList<Integer>res=new ArrayList<>();
-        int x=0;
-        for(int i=arr.length-1;i>=0;i--){
-            if(arr[i]>=x){
-                res.add(arr[i]);
-                x=arr[i];
+        ArrayList<Integer> arrayLeaders = new ArrayList<>();
+        
+        int max = Integer.MIN_VALUE;
+        for(int i = arr.length - 1; i >= 0; i--){
+            int current = arr[i];
+            if(current >= max){
+                max = current;
+                arrayLeaders.add(max);
             }
         }
-        Collections.reverse(res);
-        return res;
+        return reverse(arrayLeaders);
+    }
+    
+    static ArrayList<Integer> reverse(ArrayList<Integer> arrayLeaders){
+        int start = 0;
+        int end = arrayLeaders.size() - 1;
+        
+        while(start < end) {
+            int temp = arrayLeaders.get(start);
+            arrayLeaders.set(start, arrayLeaders.get(end));
+            arrayLeaders.set(end, temp);
+            start++;
+            end--;
+        }
+        return arrayLeaders;
     }
 }
